01252dda8af8541a371bcdbc4349a243b1b7428a
donajose22 <dona.jose@intel.com>
2024-10-28 22:41:47 +0530

    first commit

--- types.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-    readonly VITE_APP_APIGEE_DEV_SECRET: string;
-    // Add other environment variables here
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
